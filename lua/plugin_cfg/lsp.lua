7f597278de2cecf4424de4406e8483c2eea42bbb
r0se <yanhai325@gmail.com>
2024-02-22 15:49:42 +0800

    add verible find

--- lua/plugin_cfg/lsp.lua
+++ lua/plugin_cfg/lsp.lua
@@ -12,6 +12,7 @@ require("mason-lspconfig").setup({
     -- 确保安装，根据需要填写
     ensure_installed = {
         "lua_ls",
+        -- find . -name "*.sv" -o -name "*.svh" -o -name "*.v" | sort > verible.filelist
         "verible",
         "pylsp",
         -- "pyre",
