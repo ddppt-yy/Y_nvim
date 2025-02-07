dc67d7f4334b05e00b2f1b8a6c5b65dcdea36930
r0se <yanhai325@gmail.com>
2025-02-07 14:11:31 +0800

    comment chg key map

--- lua/plugin_cfg/comment.lua
+++ lua/plugin_cfg/comment.lua
@@ -2,7 +2,8 @@ require('Comment').setup(
     {
         toggler = {
             ---Line-comment toggle keymap
-            line = '<c-_>',
+            -- line = '<c-_>',
+            line = '<leader>cc',
         },
     }
 )
