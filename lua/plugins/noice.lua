5e163e3dc4e61d8156f6d6c279a24c5e4a31bbd5
home_2404 <yanhai325@gmail.com>
2026-03-09 21:28:54 +0800

    lazy mdf

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,8 @@
 -- lazy.nvim
 return {
 	"folke/noice.nvim",
-	-- event = "VeryLazy",
-	event = "InsertEnter",
+	event = "VeryLazy",
+	-- event = "InsertEnter",
 	opts = {
 		lsp = {
 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -14,7 +14,7 @@ return {
 		},
 		-- you can enable a preset for easier configuration
 		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
+			bottom_search = false, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	version = "*",
+    events = "VeryLazy",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
