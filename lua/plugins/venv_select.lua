8283fe9b8ac3381d3729a4ef138ebf3e34b8522d
r0se <yanhai325@gmail.com>
2026-07-29 09:19:45 +0800

    mdf venv_select

--- lua/plugins/venv_select.lua
+++ lua/plugins/venv_select.lua
@@ -1,23 +1,54 @@
 return {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" }, -- 依赖 Telescope UI
-    ft = "python",
-    opts = {
-        -- 在此处添加你的自定义配置
-        parents = 3,             -- 向上搜索父目录的层级数，默认为 2
-        dap_enabled = true,      -- 如需与 nvim-dap 集成，设为 true
-        search_venv_managers = true, -- 是否搜索 Poetry 等工具管理的环境，默认为 true
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+        -- "mfussenegger/nvim-dap", -- 可选，用于 DAP 集成
     },
-    -- keys = {
-    --     { "<leader>vs", "<cmd>VenvSelect<cr>",       desc = "Select Virtual Environment" },
-    --     { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Select Cached Environment" },
-    -- },
+    ft = "python",
+    branch = "main", -- 使用 regexp 分支（功能更全），如求稳定可改为 "main"
+    keys = {
+        { "<leader>vs", "<cmd>VenvSelect<cr>",       desc = "Select Virtual Environment" },
+        { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Select Cached Environment" },
+    },
+    opts = {
+        -- 基础搜索设置
+        parents = 3,                    -- 向上搜索父目录的层级数
+        name = { ".venv", "venv", "env", ".env" }, -- 常见的虚拟环境目录名
+        auto_refresh = true,            -- 保存文件时自动刷新列表
+
+        -- 搜索各种工具管理的虚拟环境
+        search_venv_managers = true,    -- 搜索 Poetry, Pipenv, Hatch 等
+        search_workspace = true,        -- 搜索工作区目录
+
+        -- DAP 调试集成
+        dap_enabled = false,
+
+        -- 通知用户切换了环境
+        notify_user_on_activate = true,
+
+        -- 使用 Telescope 主题
+        telescope_filter_type = "substring", -- 搜索过滤方式
+    },
+
+    -- 插件加载后执行
+    config = function(_, opts)
+        require("venv-selector").setup(opts)
+
+        -- 注册 ShowCurrentVenv 用户命令
+        vim.api.nvim_create_user_command("VenvShowCurrent", function()
+            local venv = require("venv-selector").get_active_venv()
+            if venv then
+                vim.notify("Current venv: " .. venv, vim.log.levels.INFO, { title = "VenvSelector" })
+            else
+                vim.notify("No venv activated", vim.log.levels.WARN, { title = "VenvSelector" })
+            end
+        end, { desc = "Show currently active Python virtual environment" })
+
+        -- 注册 DeactivateVenv 命令
+        vim.api.nvim_create_user_command("DeactivateVenv", function()
+            require("venv-selector").deactivate_venv()
+            vim.notify("Virtual environment deactivated", vim.log.levels.INFO, { title = "VenvSelector" })
+        end, { desc = "Deactivate current Python virtual environment" })
+    end,
 }
--- vim.api.nvim_create_user_command("ShowCurrentVenv", function()
---     local venv = require('venv-selector').get_active_venv()
---     if venv then
---         print("Current venv: " .. venv)
---     else
---         print("No venv activated")
---     end
--- end, {})
